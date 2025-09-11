3a1b77294a4888e9fab3112d96c7d8645f972368
yiselorena0808 <yiselorenasalzar@gmail.com>
2025-09-11 16:25:57 -0500

    Update auth_jwt.ts

--- backOrmPro/app/middleware/auth_jwt.ts
+++ backOrmPro/app/middleware/auth_jwt.ts
@@ -1,24 +1,35 @@
-/*import Jwt from 'jsonwebtoken';
-import type { HttpContext} from '@adonisjs/core/http'
+import Jwt from 'jsonwebtoken';
+import type { HttpContext } from '@adonisjs/core/http'
 
-const SECRET=process.env.jwt_secret || 'sstrict';
-export default class AuthJwt{
-    async handle({request,response}: HttpContext, next:any){
-        const authheader = request.header('Authorization');
-        if (!authheader) {
-  throw new Error('Authorization header missing')
-}
-        const token = authheader.replace('Bearer', '').trim()
-        if (!token){
-            return response.unauthorized({message:"Falta un token"})
-        } else {
-            try {
-                const jwtcoded = Jwt.verify(token, SECRET)
-                request.updateBody({authUsuario: jwtcoded})
-                await next()
-            } catch (error) {
-                return response.unauthorized({message:"Token invalido"})
-            }
-        }
+const SECRET = process.env.jwt_secret || 'sstrict';
+
+export default class AuthJwt {
+  async handle({ request, response }: HttpContext, next: any) {
+    const authheader = request.header('Authorization');
+
+    if (!authheader) {
+      return response.unauthorized({ message: 'Authorization header missing' });
     }
-}*/
+
+    const token = authheader.replace('Bearer', '').trim();
+
+    if (!token) {
+      return response.unauthorized({ message: 'Falta un token' });
+    }
+
+    try {
+      const jwtDecoded = Jwt.verify(token, SECRET) as any;
+
+      // Guardar datos del usuario en request
+      request.usuarioLogueado = {
+        id: jwtDecoded.id,
+        nombre: jwtDecoded.nombre,
+        id_empresa: jwtDecoded.id_empresa // si tu token incluye la empresa
+      };
+
+      await next();
+    } catch (error) {
+      return response.unauthorized({ message: 'Token inválido' });
+    }
+  }
+}
