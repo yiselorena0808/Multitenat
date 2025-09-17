a0ba10efb6e1f2f29857036af80fc47ada46e42e
miguel <miguelanmartinez717@gmail.com>
2025-09-17 15:42:28 -0500

    auth

--- backOrmPro/start/routes/BlogRoutes.ts
+++ backOrmPro/start/routes/BlogRoutes.ts
@@ -1,11 +1,9 @@
 import Route from "@adonisjs/core/services/router"
 import BlogController from "../../app/controller/PublicacionBlogController.js"
-import empresaMiddleware from "#middleware/EmpresaMildeware"
-import AuthMiddleware from "#middleware/auth_middleware"
+import AuthJwt from "../../app/middleware/auth_jwt.js"
 
 const blog = new BlogController()
-const auth = new AuthMiddleware()
-const empresa = new empresaMiddleware()
+const authJwt = new AuthJwt()
 
-Route.post('/crearBlog', blog.crearBlog).middleware([auth.handle.bind(auth), empresa.handle.bind(empresa)])
-Route.get('/listarBlog', blog.listarBlog).middleware([auth.handle.bind(auth), empresa.handle.bind(empresa)])
+Route.post('/crearBlog', blog.crearBlog).use(authJwt.handle.bind(authJwt))
+Route.get('/listarBlog', blog.listarBlog).use(authJwt.handle.bind(authJwt))
