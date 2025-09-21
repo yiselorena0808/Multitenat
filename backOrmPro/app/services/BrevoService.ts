c20e88451d2a12b134e5880a1daa82140d33d533
miguel <miguelanmartinez717@gmail.com>
2025-09-21 14:04:55 -0500

    correo

--- backOrmPro/app/services/BrevoService.ts
+++ backOrmPro/app/services/BrevoService.ts
@@ -8,7 +8,7 @@ apiKeyInstance.apiKey = apiKey
 const emailApi = new SibApiV3Sdk.TransactionalEmailsApi()
 
 export async function sendBrevoEmail({ to, subject, text }: { to: string, subject: string, text: string }) {
-  const sender = { email: 'no-reply@tusistema.com', name: 'Tu Sistema' }
+  const sender = { email: 'miguelanmartinez717@gmail.com', name: 'Tu Sistema' }
   const receivers = [{ email: to }]
   await emailApi.sendTransacEmail({
     sender,
